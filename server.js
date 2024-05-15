f0940a3b4a8f04040af338224eaa1a9c756b1d30
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-05-15 17:30:26 +0100

    update

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import routes from './routes/index';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware for parsing JSON bodies
+app.use(express.json());
+
+
 app.use('/', routes);
 
 app.listen(PORT, () => {
