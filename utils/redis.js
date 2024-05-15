847b0903b400b4884d0ecdd8e6f3ea33368e2bda
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-05-15 08:51:49 +0100

    utils

---
+++ utils/redis.js
@@ -0,0 +1,56 @@
+import redis from 'redis';
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+
+        // Display any errors in the console
+        this.client.on('error', (err) => {
+            console.error('Redis client error:', err);
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(reply);
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, duration, value, (err) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(true);
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(reply);
+            });
+        });
+    }
+}
+
+// Create and export an instance of RedisClient
+const redisClient = new RedisClient();
+export default redisClient;
